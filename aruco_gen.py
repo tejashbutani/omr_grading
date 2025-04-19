e4c588f459ec883fbebbd181570019c45a171dea
tejashbutani <tejasbutani78@gmail.com>
2025-04-20 00:21:02 +0530

    markers generated

--- aruco_gen.py
+++ aruco_gen.py
@@ -1,8 +1,10 @@
 import cv2
 import cv2.aruco as aruco
 
-aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_50)
+# Create dictionary
+aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
 
+# Generate markers
 for id in range(1, 5):
-    marker = aruco.drawMarker(aruco_dict, id, 200)  # 200x200 px
+    marker = aruco.generateImageMarker(aruco_dict, id, 200)  # 200x200 px
     cv2.imwrite(f"marker_{id}.png", marker)
